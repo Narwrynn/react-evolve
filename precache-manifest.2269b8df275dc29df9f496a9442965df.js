d713933bdcb5e0b22ebbd6ab308fa325f382020d
Rigel Lashley <Rigellashley@gmail.com>
2018-12-12 21:46:36 -0500

    Updates

---
+++ precache-manifest.2269b8df275dc29df9f496a9442965df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bfa2d68b536182f15e0",
+    "url": "/react-evolve/static/js/runtime~main.1bfa2d68.js"
+  },
+  {
+    "revision": "72256a45702e916d9edc",
+    "url": "/react-evolve/static/js/main.72256a45.chunk.js"
+  },
+  {
+    "revision": "3cfef6efe74b249c46ad",
+    "url": "/react-evolve/static/js/1.3cfef6ef.chunk.js"
+  },
+  {
+    "revision": "72256a45702e916d9edc",
+    "url": "/react-evolve/static/css/main.8b8388cd.chunk.css"
+  },
+  {
+    "revision": "cff6addc59a6f52d78164e326e8c885d",
+    "url": "/react-evolve/index.html"
+  }
+];
